ed96a96ea2f8c669787499f7da559cdaab4a22f1
Ricardo Jesus <rjj@ed.ac.uk>
2022-09-05 14:23:42 +0000

    Add `latest` branch to `makedepf90` package

--- var/spack/repos/builtin/packages/makedepf90/package.py
+++ var/spack/repos/builtin/packages/makedepf90/package.py
@@ -12,7 +12,9 @@ class Makedepf90(AutotoolsPackage):
 
     homepage = "https://salsa.debian.org/science-team/makedepf90"
     url = "https://salsa.debian.org/science-team/makedepf90/-/archive/debian/3.0.0-2/makedepf90-debian-3.0.0-2.tar.gz"
+    git = 'https://salsa.debian.org/science-team/makedepf90.git'
 
     maintainers = ['rj-jesus']
 
+    version('latest', branch='debian/latest')
     version('3.0.0-2', sha256='7e7b54aba06e58183e9473a437d90fa06a0dc295287a9080e28b3d754e1b79a8')
